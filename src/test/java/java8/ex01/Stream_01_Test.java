eb47cd297a4d9d996e8db346e556de98c7768743
myR3po <>
2017-11-16 17:46:06 +0100

    end Stream_01_Test.java

--- src/test/java/java8/ex01/Stream_01_Test.java
+++ src/test/java/java8/ex01/Stream_01_Test.java
@@ -24,7 +24,8 @@ public class Stream_01_Test {
 
         // TODO récupérer la liste des pizzas dont le prix est >= 1300
         // TODO utiliser l'API Stream
-        List<Pizza> result = null;
+        
+		List<Pizza> result =  pizzas.stream().filter(p -> p.getPrice() >= 1300).collect(Collectors.toList());
 
         assertThat(result, hasSize(3));
         assertThat(result, everyItem(hasProperty("price", anyOf(equalTo(1300), greaterThan(1300)))));
@@ -36,10 +37,10 @@ public class Stream_01_Test {
         List<Pizza> pizzas = new Data().getPizzas();
 
         // TODO valider si au moins une pizza à un prix >= 1300
-        Boolean result1 = null;
+        Boolean result1 = pizzas.stream().anyMatch(p -> p.getPrice() >= 1300);
 
         // TODO valider si au moins une pizza à un prix >= 2000
-        Boolean result2 = null;
+        Boolean result2 = pizzas.stream().anyMatch(p -> p.getPrice() >= 2000);
 
         assertThat(result1, is(true));
         assertThat(result2, is(false));
@@ -51,10 +52,10 @@ public class Stream_01_Test {
         List<Pizza> pizzas = new Data().getPizzas();
 
         // TODO valider que toutes les pizzas ont un prix >= 1300
-        Boolean result1 = null;
+        Boolean result1 = pizzas.stream().allMatch(p -> p.getPrice() >= 1300);
 
         // TODO valider que toutes les pizzas ont un prix >= 900
-        Boolean result2 = null;
+        Boolean result2 = pizzas.stream().allMatch(p -> p.getPrice() >= 900);;
 
         assertThat(result1, is(false));
         assertThat(result2, is(true));
@@ -67,7 +68,7 @@ public class Stream_01_Test {
         List<Pizza> pizzas = new Data().getPizzas();
 
         // TODO valider qu'aucune pizza n'a un prix >= 2000
-        Boolean result1 = null;
+        Boolean result1 = pizzas.stream().noneMatch(p -> p.getPrice() >= 2000);
 
         assertThat(result1, is(true));
     }
@@ -80,7 +81,9 @@ public class Stream_01_Test {
         // TODO récupérer toutes les commandes dont
         // TODO le prénom du client est "Johnny"
         // TODO dont au moins une pizza a un prix >= 1300
-        List<Order> result = null;
+        List<Order> result = orders.stream().filter(o -> o.getCustomer().getFirstname().equals("Johnny"))
+        		.filter(o -> o.getPizzas().stream().allMatch(p -> p.getPrice() >= 1300))
+        		.collect(Collectors.toList());
 
         assertThat(result, hasSize(1));
         assertThat(result.get(0), hasProperty("id", is(8)));
@@ -91,7 +94,7 @@ public class Stream_01_Test {
         List<Order> orders = new Data().getOrders();
 
         // TODO récupérer une commande faite par un client dont le prénom est "Sophie"
-        Optional<Order> result = null;
+        Optional<Order> result = orders.stream().filter(o -> o.getCustomer().getFirstname().equals("Sophie")).findFirst();
 
         assertThat(result.isPresent(), is(false));
     }
@@ -99,9 +102,9 @@ public class Stream_01_Test {
     @Test
     public void test_stream_max() throws Exception {
         List<Pizza> pizzas = new Data().getPizzas();
-
+        
         // TODO Trouver la pizza la plus chère
-        Optional<Pizza> result = null;
+        Optional<Pizza> result = pizzas.stream().max((p1, p2) -> p1.getPrice().compareTo(p2.getPrice()));
 
         assertThat(result.isPresent(), is(true));
         assertThat(result.get(), hasProperty("id", is(5)));
@@ -114,9 +117,9 @@ public class Stream_01_Test {
         List<Order> orders = new Data().getOrders();
 
         List<Pizza> pizzas = new Data().getPizzas();
-
+        
         // TODO Trouver la pizza la moins chère dont le prix est >= 950
-        Optional<Pizza> result = null;
+        Optional<Pizza> result = pizzas.stream().filter(p -> p.getPrice() >= 950).min((p1, p2) -> p1.getPrice().compareTo(p2.getPrice()));
 
         assertThat(result.isPresent(), is(true));
         assertThat(result.get(), hasProperty("id", is(3)));
