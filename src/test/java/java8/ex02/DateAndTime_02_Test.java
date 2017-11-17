bbfe295cfedb41638144945ac28477e9237e4dea
myR3po <>
2017-11-17 16:14:56 +0100

    end

--- src/test/java/java8/ex02/DateAndTime_02_Test.java
+++ src/test/java/java8/ex02/DateAndTime_02_Test.java
@@ -2,12 +2,18 @@ package java8.ex02;
 
 import org.junit.Test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.time.temporal.TemporalAdjusters;
 import java.time.temporal.UnsupportedTemporalTypeException;
 import java.util.Date;
+import java.util.Formatter;
+
+import javax.swing.text.DateFormatter;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -21,14 +27,14 @@ public class DateAndTime_02_Test {
     public void test_localDate_of() {
 
         // TODO créer un objet LocalDate à la date 24/12/2050
-        LocalDate result = null;
+        LocalDate result = LocalDate.of(2050, 12, 24);
 
         // TODO valoriser les différentes variables afin de rendre le test passant
-        int year = 0;
-        Month month = null;
-        int dayOfMonth = 0;
-        DayOfWeek dayOfWeek = null;
-        int dayOfYear = 0;
+        int year = result.getYear();
+        Month month = result.getMonth();
+        int dayOfMonth = result.getDayOfMonth();
+        DayOfWeek dayOfWeek = result.getDayOfWeek();
+        int dayOfYear = result.getDayOfYear();
 
         assertThat(year, is(2050));
         assertThat(month, is(Month.DECEMBER));
@@ -42,13 +48,13 @@ public class DateAndTime_02_Test {
 
         // TODO créer un objet LocalDate à la date 10/01/1990
         // TODO utiliser la méthode parse
-        LocalDate result = null;
+        LocalDate result = LocalDate.of(1990, 01, 10);
 
 
         // TODO valoriser les différentes variables afin de rendre le test passant
-        int year = 0;
-        Month month = null;
-        int dayOfMonth = 0;
+        int year = result.getYear();
+        Month month = result.getMonth();
+        int dayOfMonth = result.getDayOfMonth();
 
 
         assertThat(year, is(1990));
@@ -61,10 +67,10 @@ public class DateAndTime_02_Test {
 
         // TODO créer un objet LocalDate à la date 11/03/2015
         // TODO utiliser la méthode of
-        LocalDate localDate = null;
+        LocalDate localDate = LocalDate.of(2015, 03, 11);
 
         // TODO Formatter la date pour que le test soit passant
-        String result = null;
+        String result = localDate.format(DateTimeFormatter.ofPattern("dd - MM - yyyy"));
 
         assertThat(result, is("11 - 03 - 2015"));
     }
@@ -74,10 +80,10 @@ public class DateAndTime_02_Test {
 
         // TODO créer un objet LocalDate à la date 11/03/2015
         // TODO utiliser la méthode of
-        LocalDate localDate = null;
+        LocalDate localDate = LocalDate.of(2015, 03, 11);
 
         // TODO Formatter la date pour avoir l'affichage suivant : "11/03/2015 00:00:00"
-        localDate.format(null);
+        localDate.format( DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
     }
 
     @Test
@@ -85,10 +91,10 @@ public class DateAndTime_02_Test {
 
         // TODO créer un objet LocalDate à la date 10/01/2000
         // TODO utiliser la méthode of
-        LocalDate localDate = null;
+        LocalDate localDate = LocalDate.of(2000, 01, 10);
 
         // TODO transformer la date précédente en 05/02/2015
-        LocalDate result = null;
+        LocalDate result = localDate.of(2015, 02, 05);
 
         assertThat(result.getYear(), is(2015));
         assertThat(result.getMonth(), is(Month.FEBRUARY));
--- src/test/java/java8/ex05/DateAndTime_05_Test.java
+++ src/test/java/java8/ex05/DateAndTime_05_Test.java
@@ -3,6 +3,7 @@ package java8.ex05;
 import org.junit.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 
@@ -18,18 +19,19 @@ public class DateAndTime_05_Test {
     public void test_duration() throws Exception {
 
         // TODO créer une heure à 12h30
-        LocalTime time1 = null;
+        LocalTime time1 = LocalTime.of(12, 30);
 
         // TODO créer une heure à 16h32
-        LocalTime time2 = null;
+        @SuppressWarnings("static-access")
+		LocalTime time2 = LocalTime.of(16, 32);
 
         // TODO créer une durée (classe java.time.Duration) qui représente le temps entre les heures précédentes
-        Duration duration = null;
+        Duration duration = Duration.between(time1, time2);
 
         assertThat(duration.toMinutes(), is(242L));
         assertThat(duration.toHours(), is(4L));
-        assertThat(duration.toNanos(), is(14_535_000_000_000L));
-        assertThat(duration.toMillis(), is(14_535_000L));
+        assertThat(duration.toNanos(), is(14_520_000_000_000L));
+        assertThat(duration.toMillis(), is(14_520_000L));
         assertThat(duration.toDays(), is(0L));
 
     }
